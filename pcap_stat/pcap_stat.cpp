9eccf6a2c44980be25692ef128b82cd8dd507104
MyongKD <kdsecu@gmail.com>
2020-01-14 04:19:23 +0900

    ether map, ip map update

--- pcap_stat/pcap_stat.cpp
+++ pcap_stat/pcap_stat.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
 		std::cout << "Usage : " << argv[0] << " pcap_file_name";
 		return -1;
 	}
+
 	//pcap file error & fp 얻기
 	pcap_t* fp;
 	char errbuf[PCAP_ERRBUF_SIZE];
@@ -20,8 +21,9 @@ int main(int argc, char** argv) {
 	pcap_pkthdr* fpHeader;
 	const UINT8* data;
 	int res;
-	std::map<eth_addr, eth_endpoint> eth_map;
-	std::map<ip_addr, eth_endpoint> ip_map;
+	std::map<eth_addr, eth_stat_data> eth_EP_map;
+	std::map<ip_address, ip_stat_data> ip_EP_map;
+	
 	while ((res = pcap_next_ex(fp, &fpHeader, &data)) >= 0) {
 		//시간지연될 때(나중에 쓸까봐)
 		if (res == 0) {
@@ -36,8 +38,56 @@ int main(int argc, char** argv) {
 		ethHeader = reinterpret_cast<const eth_header*>(data);
 		offset += sizeof(ethHeader);
 		ipHeader = reinterpret_cast<const ip_header*>(data + offset);
-		//eth_map.insert()
+		
+		// Endpoint 저장
+		auto eth_EP_itr = eth_EP_map.find(ethHeader->eth_addr_src);
+		if (eth_EP_itr == eth_EP_map.end()) {
+			eth_addr eth_address;
+			eth_stat_data tx_value = { 0 };
+			eth_stat_data rx_value = { 0 };
+
+			eth_address = ethHeader->eth_addr_src;
+			tx_value.tx_Bytes += fpHeader->caplen;
+			tx_value.tx_Pack++;
+			tx_value.bytes += fpHeader->caplen;
+			tx_value.nPack++;
+
+			eth_address = ethHeader->eth_addr_dst;
+			rx_value.rx_Bytes += fpHeader->caplen;
+			rx_value.rx_Pack++;
+			rx_value.bytes += fpHeader->caplen;
+			rx_value.nPack++;
+
+			eth_EP_map.insert({ ethHeader->eth_addr_src, tx_value });
+			eth_EP_map.insert({ ethHeader->eth_addr_dst, rx_value });
+		}
+
+		auto ip_EP_itr = ip_EP_map.find(ipHeader->saddr);
+		if (ip_EP_itr == ip_EP_map.end()) {
+			ip_address ip_addr;
+			ip_stat_data tx_value = { 0 };
+			ip_stat_data rx_value = { 0 };
+
+			ip_addr = ipHeader->saddr;
+			tx_value.tx_Bytes += fpHeader->caplen;
+			tx_value.tx_Pack++;
+			tx_value.bytes += fpHeader->caplen;
+			tx_value.nPack++;
+
+			ip_addr = ipHeader->daddr;
+			rx_value.rx_Bytes += fpHeader->caplen;
+			rx_value.rx_Pack++;
+			rx_value.bytes += fpHeader->caplen;
+			rx_value.nPack++;
+
+			ip_EP_map.insert({ ipHeader->saddr, tx_value });
+			ip_EP_map.insert({ ipHeader->daddr, rx_value });
+		}
+
+		// conversation 저장
+		auto ip_CONV_itr = ip_CONV_map.find({ ipHeader->saddr, ipHeader->daddr },tx_value);
 	}
+
 	
 	pcap_close(fp);
 }
--- pcap_stat/type.h
+++ pcap_stat/type.h
@@ -1,7 +1,7 @@
 #pragma once
 #include<pcap.h>
 
-typedef UINT32 ip_addr;
+typedef UINT32 ip_address;
 typedef struct ip_header {
 	UINT8	ver_ihl;		// Version (4 bits) + Internet header length (4 bits)
 	UINT8	tos;			// Type of service 
@@ -11,13 +11,23 @@ typedef struct ip_header {
 	UINT8	ttl;			// Time to live
 	UINT8	proto;			// Protocol
 	UINT16	crc;			// Header checksum
-	UINT32	saddr;		// Source address
-	UINT32	daddr;		// Destination address
+	UINT32	saddr;			// Source address
+	UINT32	daddr;			// Destination address
 	UINT32	op_pad;			// Option + Padding
 }ip_header;
 
 typedef struct eth_address {
-	UINT8	dth_addr[6];
+	UINT8	eth_addr[6];
+
+	bool operator<(const eth_address& other) const{
+		for (int i = 0; i < 6; i++) {
+			if (eth_addr[i] < other.eth_addr[i]) return true;
+			else if (eth_addr[i] > other.eth_addr[i]) return false;
+			else continue;
+		}
+		return false;
+	}
+	
 }eth_addr;
 
 typedef struct eth_header {
@@ -26,21 +36,27 @@ typedef struct eth_header {
 	UINT16	eth_type;
 }eth_header;
 
-typedef struct eth_endpoint {
-	eth_addr address;
+typedef struct eth_stat_data {
 	UINT32	nPack;
 	UINT32	bytes;
-	UINT32	txPack;
-	UINT32	txBytes;
-	UINT32	rxPack;
-	UINT32	rxBytes;
+	UINT32	tx_Pack;
+	UINT32	tx_Bytes;
+	UINT32	rx_Pack;
+	UINT32	rx_Bytes;
 };
-typedef struct ip_endpoint {
-	UINT32 address;
+typedef struct ip_stat_data {
 	UINT32	nPack;
 	UINT32	bytes;
-	UINT32	txPack;
-	UINT32	txBytes;
-	UINT32	rxPack;
-	UINT32	rxBytes;
+	UINT32	tx_Pack;
+	UINT32	tx_Bytes;
+	UINT32	rx_Pack;
+	UINT32	rx_Bytes;
 };
+typedef struct conversation_eth {
+	eth_addr saddr;
+	eth_addr daddr;
+}conv_eth;
+typedef struct conversation_ip {
+	UINT32 saddr;
+	UINT32 daddr;
+}conv_ip;
